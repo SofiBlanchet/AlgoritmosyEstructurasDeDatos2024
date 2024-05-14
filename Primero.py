748101d40793fa271ddd842b508d30036a0957a5
SofiBlanchet <blanchetibarrasofia@gmail.com>
2024-05-14 01:55:42 -0300

    Primera entrega completa

--- Primero.py
+++ Primero.py
@@ -14,3 +14,25 @@ def usarlafuerza(mochila,objetosquitados):
     return usarlafuerza(mochila,objetosquitados)
     
 print(usarlafuerza(mochila,objetosquitados))
+
+#5
+num="CCLXV"
+numd=0
+def numeros(num, numd):
+    dic={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    if len(num)==0:
+        return "El número decimal es", numd
+    else:
+        if len(num)!=1:
+            if dic[num[1]]>dic[num[0]]:
+                a=dic[num[1]]-dic[num[0]]
+                numd+=int(a)
+                return numeros(num[2:],numd)
+            else:
+                numd+=int(dic[num[0]])
+                return numeros(num[1:],numd)
+        else: 
+            numd+=int(dic[num[0]])
+            return numeros (num[1:],numd)
+    
+print(numeros(num,numd))
